2f639e36d5ef0ac67175dc8fcb24a71264ec8259
Fatima Alhasan Salisu <mamannurain20@gmail.com>
2025-10-30 11:26:35 +0100

    cloudinary,routes, correction

--- backend/routes/flowerRoutes.js
+++ backend/routes/flowerRoutes.js
@@ -7,10 +7,11 @@ const cloudinary = require("cloudinary").v2;
 
 // ✅ Cloudinary config
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
+
 
 // ✅ Use Cloudinary as multer storage
 const storage = new CloudinaryStorage({
